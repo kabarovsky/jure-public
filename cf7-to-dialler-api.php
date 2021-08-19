224f8d6e1da624d2f3d2ff7149310bbaffc28970
Jure Mraović <mraovic.jure@gmail.com>
2021-08-19 14:36:50 +0200

    Create cf7-to-dialler-api.php

---
+++ cf7-to-dialler-api.php
@@ -0,0 +1,87 @@
+<?php
+
+add_action( 'wpcf7_before_send_mail', 
+
+    function( $contact_form, $abort, $submission ) {
+
+        $form_ids = array('181','268');
+        $form_id = $_POST['_wpcf7'];
+        
+        if ( in_array($form_id, $form_ids) ) {
+
+            $last_name = $submission->get_posted_data( 'full-name' );
+            $email_address = $submission->get_posted_data( 'email-address');
+            $phone_number = $submission->get_posted_data( 'phone-number');
+
+            // Login Request
+
+            $curl_login = curl_init();
+
+            curl_setopt_array($curl_login, array(
+                CURLOPT_URL => 'https://api5.cnx1.uk/consumer/login',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('username' => 'PinpointCallSolutions','password' => '0329999999vn78vnvnvnvnnvbnvbm'),
+            ));
+
+            $response_login = curl_exec($curl_login);
+            curl_close($curl_login);
+
+            $response_json = json_decode($response_login);
+            $response_token = $response_json->token;
+
+            // Create Request
+
+            $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+            $curl_create = curl_init();
+
+            curl_setopt_array($curl_create, array(
+                CURLOPT_URL => 'https://api5.cnx1.uk/customer/create',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_USERAGENT => $user_agent,
+                CURLOPT_POSTFIELDS =>'{
+                    "first_name": "{{first_name}}",
+                    "last_name": " ' . $last_name . ' ",
+                    "email": " ' . $email_address . ' ",
+                    "date_of_birth": "{{date_of_birth}}",
+                    "main_phone": " ' . $phone_number . ' ",
+                    "phone_code": "44",
+                    "data_list": 1020,
+                    "custom_fields": {
+                    "utm_source": "{{utm_source}}",
+                    "utm_campaign": "{{utm_campaign}}",
+                    "utm_medium":"{{utm_medium}}",
+                    "utm_term":"{{utm_term}}",
+                    "utm_content":"{{utm_content}}"
+                    },
+                    "auto_queue":true,
+                    "queue_priority":99,
+                    "token": "FCYK5vHumrtd9bOXyLiqbHUzIWA4eqQuVBESGL70dPAbZ1zpIB"
+                }',
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json',
+                    'Authorization: Bearer ' . $response_token
+                ),
+            ));
+
+            $response_create = curl_exec($curl_create);
+
+            curl_close($curl_create);
+
+        }
+
+  }, 10, 3
+
+);
